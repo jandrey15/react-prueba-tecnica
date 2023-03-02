d6c9b438e91c604615ac388c0c9d7f1179f002e8
John Serrano <andrey_201015@hotmail.com>
2023-03-02 15:55:38 -0500

    Add fetch catass with three firts words

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
 import { useEffect, useState } from 'react'
 
 const CAT_ENDPOINT_RANDOM_FACTORY = 'https://catfact.ninja/fact'
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export function App () {
   const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACTORY)
       .then(res => res.json())
-      .then(data => setFact(data.fact))
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        const threeFirstWords = fact.split(' ', 3).join(' ')
+        console.log(threeFirstWords)
+
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?json=true`)
+          .then(res => res.json())
+          .then(response => {
+            console.log(response)
+            const { url } = response
+            setImageUrl(url)
+          })
+      })
   }, [])
 
   return (
     <main>
       <h1>App de gatitos</h1>
       {fact && <p>{fact}</p>}
+      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted the first three words for ${fact}`} />}
     </main>
   )
 }
